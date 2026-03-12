6b4697d55c6360fe8c043e7b6209a3a1d6527c61
Arbab Mujtaba <arbabmujtaba@Arbabs-MacBook-Air.local>
2026-03-12 13:35:54 +0530

    origin main

---
+++ src/IfElse/RelationalOperators.java
@@ -0,0 +1,9 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        System.out.println(5<4);
+        System.out.println(6*5 == 30);
+        System.out.println(55<=55);
+        System.out.println(5<4);
+        System.out.println(5<4);
+    }
+}
